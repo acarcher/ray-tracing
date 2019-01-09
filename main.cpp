ff91096b53b788872dd410ae0d6698fa0ca724f7
Alexander Archer <aca3936@gmail.com>
2019-01-09 13:56:01 -0500

    Split camera into own class and header file Added multiple samples per pixel for anti-aliasing

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "float.h"
+#include "camera.h"
 
 vec3 color(const ray& r, hitable *world) {
     hit_record rec;
@@ -19,12 +20,9 @@ vec3 color(const ray& r, hitable *world) {
 int main() {
     int nx = 200;
     int ny = 100;
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    int ns = 100;
 
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
     hitable *list[2];
     list[0] = new sphere(vec3(0, 0, -1), 0.5);
@@ -32,16 +30,23 @@ int main() {
 
     hitable *world = new hitable_list(list, 2);
 
+    camera cam;
+
     for (int j = ny - 1; j >= 0; j--){
         for (int i = 0; i < nx; i++){
+            vec3 col(0, 0, 0);
 
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
 
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+                ray r = cam.get_ray(u, v);
 
-            vec3 p = r.point_at_parameter(2.0);
-            vec3 col = color(r, world);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world);
+            }
+
+            col /= float(ns);
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
